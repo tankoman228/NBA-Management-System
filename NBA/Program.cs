0120d34b558b08e19fb6734afd6957407a339fe2
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2023-03-13 10:10:13 +0700

    Empty forms

--- NBA/Program.cs
+++ NBA/Program.cs
@@ -15,7 +15,7 @@ namespace NBA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainScreen());
         }
     }
 }
